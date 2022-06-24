83554810eed64521940bd1af7f7f23fb4f0775eb
swayampandey <swayam265@gmail.com>
2022-06-24 14:57:35 +0530

    Added: UC2

---
+++ UC2.java
@@ -0,0 +1,18 @@
+
+public class UC2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int playerposition=0;
+		System.out.println("Welcome to Snake and Ladder Game");
+		
+		System.out.println("Created by Swayam Prakash");
+		
+		System.out.println("Player Starting Position is " +playerposition);
+		
+		int number = 1+ (int) (Math.random()*6);
+		System.out.println("After rolling the dice player 1 got =  " +number);
+
+	}
+
+}
