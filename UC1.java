4cf2918cbaae599392df479e336ca494ffe36aed
swayampandey <swayam265@gmail.com>
2022-06-24 14:54:30 +0530

    Initial commit

---
+++ UC1.java
@@ -0,0 +1,16 @@
+
+public class UC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int playerposition=0;
+		System.out.println("Welcome to Snake and Ladder Game");
+		
+		System.out.println("Created by Swayam Prakash");
+		
+		System.out.println("Player Starting Position is " +playerposition);
+		
+
+	}
+
+}
